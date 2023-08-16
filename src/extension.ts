5ffcf91d501d36dc4ec67ed6752428250689633b
Michaell Alavedra <37460957+gitchaell@users.noreply.github.com>
2023-08-16 17:30:58 +0000

    prepare UI

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,34 @@
 import * as vscode from 'vscode';
-import { ExampleProvider } from './app/infrastructure/providers/ExampleProvider';
+import {
+	PortNode,
+	PortProvider,
+} from './app/infrastructure/providers/PortProvider';
 
 export function activate(context: vscode.ExtensionContext) {
-	vscode.window.registerTreeDataProvider('exampleList', new ExampleProvider());
+	const portProvider = new PortProvider();
 
-	// vscode.window.createTreeView('exampleList', {
-	// 	treeDataProvider: new ExampleProvider(),
-	// });
+	vscode.window.registerTreeDataProvider('portman', portProvider);
+
+	const view = vscode.window.createTreeView('portman', {
+		treeDataProvider: portProvider,
+	});
+
+	vscode.commands.registerCommand('portman.refresh', () =>
+		portProvider.refresh()
+	);
+
+	vscode.commands.registerCommand('portman.kill', () => {
+		// TODO: request port number input
+
+		// if (!node) {
+		// 	vscode.window.showErrorMessage('Port not selected');
+		// 	return;
+		// }
+
+		// vscode.window.showInformationMessage(`Killing port <${node.label}>`);
+	});
+
+	context.subscriptions.push(view);
 }
 
 export function deactivate() {}
