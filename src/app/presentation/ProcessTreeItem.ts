62980f1c0588fc2d51c15c526dd62b1f2dbb5eea
Michaell Alavedra <37460957+gitchaell@users.noreply.github.com>
2023-08-22 01:20:14 +0000

    fix: uuid for tree items

--- src/app/presentation/ProcessTreeItem.ts
+++ src/app/presentation/ProcessTreeItem.ts
@@ -1,12 +1,13 @@
 import { ThemeIcon, TreeItem, TreeItemCollapsibleState } from 'vscode';
 
+import { Uuid } from '../../shared/domain/value-object/Uuid';
 import { Process } from '../domain/Process';
 
 export class ProcessTreeItem extends TreeItem {
 	constructor(public readonly process: Process) {
 		super(process.label, TreeItemCollapsibleState.None);
 
-		this.id = process.id.value;
+		this.id = Uuid.random().value;
 		this.description = process.description;
 		this.tooltip = process.tooltip;
 
