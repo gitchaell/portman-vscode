e14239522c97703b0ca1aff2a3913973f6ac4391
Michaell Alavedra <37460957+gitchaell@users.noreply.github.com>
2024-03-19 14:46:54 +0000

    chore: organize directory

---
+++ src/infrastructure/linux/LinuxProcessRepository.ts
@@ -0,0 +1,53 @@
+import { exec } from 'node:child_process';
+import { promisify } from 'node:util';
+
+import { Process } from '@/domain/Process';
+import { ProcessRepository } from '@/domain/ProcessRepository';
+import { LinuxProcessTransformer } from './LinuxProcessTransformer';
+import { CommandExecutionError } from '@/shared/domain/exceptions/CommandExecutionError';
+
+const execute = promisify(exec);
+const command = {
+	isRoot: () => `sudo -h`,
+	getAll: (asRoot = false) =>
+		`${
+			asRoot ? 'sudo ' : ''
+		}netstat --numeric --listening --program --tcp --udp | grep LISTEN | awk -v OFS='(|)' '{print $1, $2, $3, $4, $5, $6, $7}'`,
+	kill: (pid: string, asRoot = false) => `${asRoot ? 'sudo ' : ''}kill ${pid}`,
+};
+
+export class LinuxProcessRepository implements ProcessRepository {
+	async search(): Promise<Process[]> {
+		const result = execute(command.isRoot()).then(async ({ stdout }) => {
+			const asRoot = !!stdout;
+
+			return execute(command.getAll(asRoot)).then(({ stdout, stderr }) => {
+				if (stderr) {
+					throw new CommandExecutionError(
+						`The command executed has failed. ${stderr}`
+					);
+				}
+
+				const transformer = new LinuxProcessTransformer();
+				const ports = transformer.transform(stdout);
+
+				return ports;
+			});
+		});
+
+		return result;
+	}
+
+	async kill(process: Process): Promise<void> {
+		execute(command.isRoot()).then(({ stdout }) => {
+			const asRoot = !!stdout;
+			execute(command.kill(process.id.value, asRoot)).then(({ stderr }) => {
+				if (stderr) {
+					throw new CommandExecutionError(
+						`The command executed has failed. ${stderr}`
+					);
+				}
+			});
+		});
+	}
+}
---
+++ src/infrastructure/windows/WindowsProcessRepository.ts
@@ -0,0 +1,36 @@
+import { exec } from 'node:child_process';
+import { promisify } from 'node:util';
+
+import { CommandExecutionError } from '@/shared/domain/exceptions/CommandExecutionError';
+import { ProcessRepository } from '@/domain/ProcessRepository';
+import { Process } from '@/domain/Process';
+import { WindowsProcessTransformer } from './WindowsProcessTransformer';
+
+const execute = promisify(exec);
+const command = {
+	getAll: () => `netstat -a -b -n -o | findstr LISTENING`,
+	kill: (pid: string) => `taskkill /PID ${pid}`,
+};
+
+export class WindowsProcessRepository implements ProcessRepository {
+	async search(): Promise<Process[]> {
+		execute(command.getAll()).then(({ stdout, stderr }) => {
+			if (stderr) {
+				throw new CommandExecutionError(
+					`The command executed has failed. ${stderr}`
+				);
+			}
+
+			const transformer = new WindowsProcessTransformer();
+			const ports = transformer.transform(stdout);
+
+			return ports;
+		});
+
+		return [];
+	}
+
+	async kill(process: Process): Promise<void> {
+		return execute(command.kill(process.id.value)).then(console.log);
+	}
+}
